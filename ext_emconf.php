2cf5f340c23b4199c2474497bd3fdddaa59ff512
mattias.nilsson <mattias.nilsson@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-15 12:33:07 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mn_twitter_bootstrap/trunk@57968 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mn_twitter_bootstrap".
 #
-# Auto generated 15-02-2012 08:18
+# Auto generated 15-02-2012 11:46
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"d9e1";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"84ea";s:20:"doc/wizard_form.html";s:4:"58d9";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"d9e1";s:10:"README.txt";s:4:"ee2d";s:31:"class.tx_mntwitterbootstrap.php";s:4:"6f40";s:16:"ext_autoload.php";s:4:"a145";s:21:"ext_conf_template.txt";s:4:"d799";s:12:"ext_icon.gif";s:4:"5a78";s:17:"ext_localconf.php";s:4:"8b8a";s:14:"ext_tables.php";s:4:"a263";s:14:"doc/manual.pdf";s:4:"dee6";s:14:"doc/manual.sxw";s:4:"4c07";s:19:"doc/wizard_form.dat";s:4:"84ea";s:20:"doc/wizard_form.html";s:4:"58d9";s:42:"res/bootstrap/css/bootstrap-responsive.css";s:4:"86da";s:46:"res/bootstrap/css/bootstrap-responsive.min.css";s:4:"2f5c";s:31:"res/bootstrap/css/bootstrap.css";s:4:"7232";s:35:"res/bootstrap/css/bootstrap.min.css";s:4:"18ff";s:48:"res/bootstrap/img/glyphicons-halflings-white.png";s:4:"1111";s:42:"res/bootstrap/img/glyphicons-halflings.png";s:4:"531d";s:29:"res/bootstrap/js/bootstrap.js";s:4:"7aff";s:33:"res/bootstrap/js/bootstrap.min.js";s:4:"65aa";}',
+	'suggests' => array(
+	),
 );
 
 ?>
