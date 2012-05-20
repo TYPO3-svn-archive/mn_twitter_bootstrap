b472f657f90ba2b809f99aef78c2d4094b930cad
mattias.nilsson <mattias.nilsson@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-20 14:03:06 +0000

    TER release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mn_twitter_bootstrap/trunk@62533 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mn_twitter_bootstrap".
 #
-# Auto generated 16-02-2012 08:04
+# Auto generated 20-05-2012 13:58
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"d9e1";s:10:"README.txt";s:4:"ee2d";s:31:"class.tx_mntwitterbootstrap.php";s:4:"a496";s:16:"ext_autoload.php";s:4:"a145";s:21:"ext_conf_template.txt";s:4:"d799";s:12:"ext_icon.gif";s:4:"5a78";s:17:"ext_localconf.php";s:4:"8b8a";s:14:"ext_tables.php";s:4:"a263";s:14:"doc/manual.pdf";s:4:"dee6";s:14:"doc/manual.sxw";s:4:"58dd";s:19:"doc/wizard_form.dat";s:4:"84ea";s:20:"doc/wizard_form.html";s:4:"58d9";s:42:"res/bootstrap/css/bootstrap-responsive.css";s:4:"86da";s:46:"res/bootstrap/css/bootstrap-responsive.min.css";s:4:"2f5c";s:31:"res/bootstrap/css/bootstrap.css";s:4:"7232";s:35:"res/bootstrap/css/bootstrap.min.css";s:4:"18ff";s:48:"res/bootstrap/img/glyphicons-halflings-white.png";s:4:"1111";s:42:"res/bootstrap/img/glyphicons-halflings.png";s:4:"531d";s:29:"res/bootstrap/js/bootstrap.js";s:4:"7aff";s:33:"res/bootstrap/js/bootstrap.min.js";s:4:"65aa";s:38:"res/template/typo3-intro-template.html";s:4:"3aa3";}',
+	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"d9e1";s:10:"README.txt";s:4:"ee2d";s:31:"class.tx_mntwitterbootstrap.php";s:4:"06ed";s:16:"ext_autoload.php";s:4:"a145";s:21:"ext_conf_template.txt";s:4:"b894";s:12:"ext_icon.gif";s:4:"5a78";s:17:"ext_localconf.php";s:4:"8b8a";s:14:"ext_tables.php";s:4:"a263";s:14:"doc/manual.pdf";s:4:"dee6";s:14:"doc/manual.sxw";s:4:"58dd";s:19:"doc/wizard_form.dat";s:4:"84ea";s:20:"doc/wizard_form.html";s:4:"58d9";s:42:"res/bootstrap/css/bootstrap-responsive.css";s:4:"f9de";s:46:"res/bootstrap/css/bootstrap-responsive.min.css";s:4:"18ed";s:31:"res/bootstrap/css/bootstrap.css";s:4:"ab31";s:35:"res/bootstrap/css/bootstrap.min.css";s:4:"2407";s:48:"res/bootstrap/img/glyphicons-halflings-white.png";s:4:"9bbc";s:42:"res/bootstrap/img/glyphicons-halflings.png";s:4:"74b8";s:29:"res/bootstrap/js/bootstrap.js";s:4:"b41c";s:33:"res/bootstrap/js/bootstrap.min.js";s:4:"71e1";s:29:"res/css/navbar-responsive.css";s:4:"8017";s:38:"res/template/typo3-intro-template.html";s:4:"3aa3";}',
 	'suggests' => array(
 	),
 );
